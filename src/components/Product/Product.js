fe59ba507f22b7f28e017424ab6a551dd9e2ec85
Sazzads <sazzadtk@gmail.com>
2022-03-26 21:19:43 +0600

    final commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,13 +7,14 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 const Product = (props) => {
     const { product, handleAddToCart } = props;
 
-    const { name, img, price } = props.product;
+    const { id, name, img, price } = props.product;
 
     return (
         <div className='product'>
             <img src={img} alt=""></img>
             <div className="product-info">
                 <p className='product-name'>Name: {name}</p>
+                <p>ID: {id}</p>
                 <p>Price:${price}</p>
             </div>
             <button onClick={() => handleAddToCart(product)} className='btn-cart'>
